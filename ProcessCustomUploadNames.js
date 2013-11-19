79097993bc4895d8b7798a0b3334996ae044a989
adrianbj <adrianbjones@gmail.com>
2013-11-19 22:39:39 +1000

    Hopefully better fix for ASM issue

--- ProcessCustomUploadNames.js
+++ ProcessCustomUploadNames.js
@@ -6,13 +6,11 @@ $(document).ready(function() {
 	var addRule = function(e) {
 		e.preventDefault();
 		$(this).toggleClass('ui-state-active');
-		var newRow = $('<li class="Inputfield InputfieldWrapper InputfieldColumnWidthFirst">').load('?addRule=' + ($('#RenameRules ul.Inputfields ul.Inputfields').length));
+		var newRow = $('<li class="Inputfield InputfieldWrapper InputfieldColumnWidthFirst">').load('?addRule=' + ($('#RenameRules ul.Inputfields ul.Inputfields').length), function() {
+ 			$(newRow).find(".InputfieldAsmSelect select[multiple=multiple]").asmSelect(options);
+		});
 		$(this).prev('ul:first').append(newRow);
 		var options = { sortable: false };
-		// ugly hack to wait for new select fields to be ready before converting them to ASM fields. This seems to work most of the time, but still not 100% ?
-		setTimeout(function(){
-			$(newRow).find(".InputfieldAsmSelect select[multiple=multiple]").asmSelect(options);
-		},200);
 	};
 
     if ($.isFunction($(document).on)) {
