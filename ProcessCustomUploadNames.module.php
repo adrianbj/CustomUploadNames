579fa0fc2ae64e51b1bafe98801d87f9c9602744
Adrian Jones <adrianbjones@gmail.com>
2026-04-06 04:47:48 -0700

    Bump version number

--- ProcessCustomUploadNames.module.php
+++ ProcessCustomUploadNames.module.php
@@ -22,7 +22,7 @@ class ProcessCustomUploadNames extends WireData implements Module, ConfigurableM
     public static function getModuleInfo() {
         return array(
             'title' => __('Custom Upload Names'),
-            'version' => '1.4.0',
+            'version' => '1.4.1',
             'author' => 'Adrian Jones',
             'summary' => __('Automatically rename file/image uploads according to a configurable format'),
             'href' => 'http://modules.processwire.com/modules/process-custom-upload-names/',
